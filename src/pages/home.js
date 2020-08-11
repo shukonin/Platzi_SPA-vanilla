6d3857577c945021215108821162278eb34334d2
shukonin <fernando.canas@outlook.com>
2020-08-10 20:43:42 -0500

    index.js, index.html & home.js complete

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    const view = `
+    <div class="Characters">
+        <article class="Character-item">
+            <a href="#/1/"></a>
+            <img src="image" alt="name">
+            <h2>Name</h2>
+        </article>
+    </div>
+    `
+    return view;
+}
+
+export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { resolve } = require('path');
 
 module.exports = {
-    entry: '.src/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
