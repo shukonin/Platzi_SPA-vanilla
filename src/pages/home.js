d3bd7040d5b658ac7fce66cb3568d3a1cf49f534
shukonin <fernando.canas@outlook.com>
2020-08-10 21:48:35 -0500

    rounting and rendering working

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,9 +2,10 @@ const Home = () => {
     const view = `
     <div class="Characters">
         <article class="Character-item">
-            <a href="#/1/"></a>
-            <img src="image" alt="name">
-            <h2>Name</h2>
+            <a href="#/1/">
+                <img src="image" alt="name">
+                <h2>Name</h2>
+            </a>
         </article>
     </div>
     `
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ import Error404 from '../pages/Error404'
 import getHash from '../utils/getHash'
 import resolveRoutes from '../utils/resolveRoutes'
 
-const Routes = {
+const routes = {
     '/': Home,
     '/:id': Character,
     '/contact': 'Contact',
@@ -16,6 +16,11 @@ const router = async () => {
     const content = null || document.getElementById('content')
 
     header.innerHTML = await Header()
+    let hash = getHash()
+    let route = await resolveRoutes(hash)
+    let render = routes[route] ? routes[route] : Error404
+    content.innerHTML = await render()
+
 }
 
 export default router
